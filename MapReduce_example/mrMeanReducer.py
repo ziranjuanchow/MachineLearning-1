faef41bc2e3958c17e24b889748e464c0664e9d3
1mrliu <>
2018-05-18 15:28:03 +0800

    增加mapreduce函数的Python实现 实现 reducer 功能

---
+++ MapReduce_example/mrMeanReducer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+__author__ = 'liudong'
+__date__ = '2018/5/18 下午3:00'
+import sys
+from numpy import mat, mean, power
+
+def read_input(file):
+    for line in file:
+        # yield相当于一个一个的输出
+        yield line.rstrip()
+input  = read_input(sys.stdin)
+mapperOut = [line.split('\t') for line in input]
+cumVal = 0.0
+cumSumSq = 0.0
+cumN = 0.0
+for instance in mapperOut:
+    # print(instance[0])
+    # print(instance[1])
+    # print(instance[2])
+    nj = 70.0
+    cumN += nj
+    cumVal += nj*float(instance[1])
+    cumSumSq += nj*float(instance[2])
+mean = cumVal / cumN
+varSum = (cumSumSq - 2*mean*cumVal + cumN*mean*mean)/cumN
+print('%d\t%f\t\%f' % (cumN, mean, varSum))
+print(sys.stderr, "report: still alive!")
