c1eab54a8cfb16274236e8e937db4c21ea207de0
1mrliu <>
2018-10-11 20:34:30 +0800

    add leetcode twosum solution

---
+++ LeetCode/1-TwoSum.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__author__ = 'liudong'
+__date__ = '2018/10/11 8:13 PM'
+
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+def twoSum(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+    return []
+numbers = [2, 7, 11, 15]
+target = 9
+print(twoSum(numbers,target))
