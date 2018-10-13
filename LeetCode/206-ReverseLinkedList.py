64f87546fcd9f4ebef25c77987099f92e5751919
1mrliu <>
2018-10-13 16:36:41 +0800

    add leetcode reverse Linked list solution

---
+++ LeetCode/206-ReverseLinkedList.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = 'liudong'
+__date__ = '2018/10/13 4:28 PM'
+'''
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+'''
+
+def reverseList(self,head):
+    cur, prev = head, None
+    while cur:
+        cur.next, prev, cur = prev, cur, cur.next
+    return prev
