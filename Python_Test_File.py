228ad1a7a188d01351d8a8c42d3af0c69a4ac701
1mrliu <>
2018-10-14 20:03:01 +0800

    add python_test_file , it contains some useful python method

---
+++ Python_Test_File.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+__author__ = 'liudong'
+__date__ = '2018/10/14 8:00 PM'
+
+class Me(object):
+    def __init__(self,name,hobby):
+        self.name = name
+        self.hobby = hobby
+    # def __str__(self):
+    #     return ('__str__():'+self.name+' '+self.hobby)
+    def __repr__(self):
+        return ('__repr__():'+ self.name + ' '+ self.hobby)
+
+if __name__ == "__main__":
+    me = Me('liudong','game')
+    me
+    print(me)
+    name =  'Alice ll jjk kk ds hhn'
+    last = name.split()
+    print(last)
+    girls = ['alice', 'bernice', 'clarice']
+    boys =  ['chris','arnold','bob']
+    letterGirls = {}
+    for girl in girls:
+        letterGirls.setdefault(girl[0], []).append(girl)
+        print(letterGirls)
+    print(b+'+'+g for b in boys for g in letterGirls[b[0]])
+    exec("print('Hello !')")
+    print([i for i in range(8)])
