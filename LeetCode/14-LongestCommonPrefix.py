3320a0f14a7fad6f3aea5157e02547158c2b9806
1mrliu <ld815626@hotmail.com>
2018-10-16 20:10:45 +0800

    add 14-LongestCommonPrefix bruce_force method solution
    Signed-off-by: 1mrliu <ld815626@hotmail.com>

--- LeetCode/14-LongestCommonPrefix.py
+++ LeetCode/14-LongestCommonPrefix.py
@@ -16,14 +16,31 @@ Input: ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
 '''
+import re
+'''
+使用re.match(prefix，strs[i]) 
+对prefix,strs[i]进行匹配
+ 当不匹配的时候，减少prefix的长度进行继续匹配
+ 最后输出减少后的prefix的值
+'''
 class Solution:
     def longestCommonPrefix(self, strs):
         """
         :type strs: List[str]
         :rtype: str
         """
-        
-
+        if not strs:
+            return ''
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            match = re.match(prefix,strs[i])
+            while not match:
+                prefix = prefix[:-1]
+                match = re.match(prefix,strs[i])
+        return prefix
 
 if __name__ == "__main__":
     solution = Solution()
+    strs = ["flower","flow","flight"]
+    result = solution.longestCommonPrefix(strs)
+    print(result)
