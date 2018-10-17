0e2138199315d657f53e5fb20540cfc48c8157d3
1mrliu <>
2018-10-17 11:45:55 +0800

    add 20-ValidParentheses stack method solution
    Signed-off-by: 1mrliu <>

---
+++ LeetCode/20-ValidParentheses.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+__author__ = 'liudong'
+__date__ = '2018/10/17 11:13 AM'
+'''
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+'''
+# 构建一个栈的形式进行处理
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {")":"(", "}":"{","]":"["}
+        for i in s:
+            if i in mapping: # 接下来将要加入的数据进行判断
+                # 将当前栈顶元素进行弹出 判断弹出的结果是否是在mapping中
+                top_element = stack.pop() if stack else "#"
+                if mapping[i] != top_element:
+                    return False
+            else:
+                stack.append(i)
+        return not stack
+
+if __name__  == "__main__":
+    solution = Solution()
+    s = "()[]{}"
+    result = solution.isValid(s)
+    print(result)
